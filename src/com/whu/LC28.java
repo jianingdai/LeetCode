134cf61d0feea7125c2ce1da16cd64c3a7fe3a87
jianing_dai <752442633@qq.com>
2024-09-10 22:11:35 +0800

    2024_09_10_22_11_19 lc28

---
+++ src/com/whu/LC28.java
@@ -0,0 +1,16 @@
+package com.whu;
+
+import com.whu.LC1_10.lc4;
+
+public class LC28 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+
+    public static class Solution {
+        //        kmp
+        public int strStr(String haystack, String needle) {
+            return haystack.indexOf(needle);
+        }
+    }
+}
