f84dfc06f8207ea62a45f2a2021e2bdd7f928981
Anonymous <87709342+hello00wolrd@users.noreply.github.com>
2025-01-20 17:01:25 +0800

    2024_09_11_10_58_57 lc30

---
+++ src/com/whu/LC71_80/LC80.java
@@ -0,0 +1,10 @@
+package com.whu.LC71_80;
+
+public class LC80 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+
+    private static class Solution {
+    }
+}
--- src/com/whu/Main.java
+++ src/com/whu/Main.java
@@ -7,7 +7,13 @@ public class Main {
 
     private static class Solution {
         public int removeDuplicates(int[] nums) {
-
+            int res = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (i == 0 || nums[i] != nums[i - 1]) {
+                    nums[res++] = nums[i];
+                }
+            }
+            return res;
         }
     }
 }
